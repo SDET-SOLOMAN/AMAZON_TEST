57309612c13f9892805ae0579ca51258fc045c64
Vlad Guliakin <vladislav.gulyakin@gmail.com>
2024-05-09 23:33:03 -0400

    Initial commit, files and dirs created

---
+++ locators/groceries_locators.py
@@ -0,0 +1,4 @@
+
+
+class GroceriesLocators:
+    pass
--- src/urls.py
+++ src/urls.py
@@ -1,3 +1,4 @@
 
 class Urls:
     base_url = "https://www.amazon.com/"
+    groceries_url = "https://www.amazon.com/fmc/learn-more?ref_=nav_cs_groceries"
