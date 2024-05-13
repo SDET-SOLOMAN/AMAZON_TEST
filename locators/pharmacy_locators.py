d528efa2a452edb86b19b36143f9316aac543022
Liliana <lilianakoric@gmail.com>
2024-05-13 14:27:29 -0700

    added pharmacy_TC_003.002.new

--- locators/pharmacy_locators.py
+++ locators/pharmacy_locators.py
@@ -6,6 +6,5 @@ class PharmacyLocators:
     ONE_MEDICAL = ("xpath", "//*[@title='prime one medical']")
     SIGN_UP_SIGN_IN = ("xpath", "//*[@aria-label='Sign up']")
     SIGN_IN_FORM = ("xpath", "//*[@class='a-section a-spacing-base']")
-
-
-
+    PRIME_SAVINGS_TITLE = ("xpath", "//*[@id='primesavings']//h3/strong")
+    PRIME_SAVINGS_BOX = ("xpath", "//a[@href='pricing?ref_=sf2_primesavings_t1#primesavings']")
--- pages/pharmacy_page.py
+++ pages/pharmacy_page.py
@@ -1,3 +1,4 @@
+
 from conftest import driver
 from locators.pharmacy_locators import PharmacyLocators
 from pages.base_page import BasePage
@@ -17,3 +18,10 @@ class PharmacyPage(BasePage):
 
     def check_element_is_visible(self):
         pass
+
+    def prime_savings(self):
+        self.element_is_clickable(self.locators.PRIME_SAVINGS_BOX).click()
+
+    def check_prime_savings_is_displayed(self):
+        prime_saving = self.element_is_visible(self.locators.PRIME_SAVINGS_TITLE)
+        return self.element_is_displayed(prime_saving)
