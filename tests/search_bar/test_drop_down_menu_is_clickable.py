ac1c66d4b4dec0c9f151cceae8deb084415da998
Kristina-Fisher <kristen.aka999@gmail.com>
2024-05-14 20:55:13 -0400

    Add allure to test

--- tests/search_bar/test_drop_down_menu_is_clickable.py
+++ tests/search_bar/test_drop_down_menu_is_clickable.py
@@ -9,16 +9,20 @@ from pages.search_bar_page import SearchBarPage
 from src.urls import Urls
 from locators.search_bar_locators import SearchBarLocators
 
-
+@allure.epic("Test Drop-down menu in the search box")
 class TestSearchBar:
     url = Urls()
     drop_down_locators = SearchBarLocators()
 
+    @pytest.mark.xfail
+    @allure.title("Verify that the drop-down menu in the search box is clickable")
+    @allure.severity(allure.severity_level.NORMAL)
     def test_drop_down_menu(self, driver, captcha):
-
         page = SearchBarPage(driver, self.url.base_url)
-        page.open()
-        page.navigate_and_click_on_search_field()
+        with allure.step("Navigate ti the home page"):
+            page.open()
+        with allure.step("Click on the search field"):
+            page.navigate_and_click_on_search_field()
 
         search_suggestions = SearchBarLocators.SELECT_ITEM_LIST
         if len(search_suggestions) >= 3:
