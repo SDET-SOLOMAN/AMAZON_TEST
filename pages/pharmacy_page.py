5d982b805f728e9d1c8249ab1075d4c1b63ec51e
Liliana <lilianakoric@gmail.com>
2024-05-15 21:26:45 -0700

    pharmacy_TC_003.002.new_Title

--- pages/pharmacy_page.py
+++ pages/pharmacy_page.py
@@ -1,4 +1,4 @@
-
+import allure
 from conftest import driver
 from locators.pharmacy_locators import PharmacyLocators
 from pages.base_page import BasePage
@@ -8,6 +8,7 @@ from src.urls import Urls
 class PharmacyPage(BasePage):
     locators = PharmacyLocators
     url = Urls()
+    page = BasePage
 
     def navigate_to_pharmacy(self, driver):
         self.element_is_visible(self.locators.SIGN_UP_SIGN_IN).click()
@@ -25,3 +26,4 @@ class PharmacyPage(BasePage):
     def check_prime_savings_is_displayed(self):
         prime_saving = self.element_is_visible(self.locators.PRIME_SAVINGS_TITLE)
         return self.element_is_displayed(prime_saving)
+
